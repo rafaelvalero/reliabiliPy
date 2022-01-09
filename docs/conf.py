cc450050e506db5e8596c7b6d24ee9c37d309baa
Rafael Valero-Fernandez <rafaelvalerofernandez@gmail.com>
2022-01-09 13:37:51 +0000

    playing with sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -31,8 +31,11 @@ extensions = [
     "sphinx_rtd_theme",
     "sphinx.ext.autodoc",
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages'
+    'sphinx.ext.githubpages',
+    'sphinx.ext.autosummary'
 ]
+# Turn on sphinx.ext.autosummary
+autosummary_generate = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
